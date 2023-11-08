ad0afa6820e54746db747a618b4baf25bdb6e329
apolo-sys <cristian124421@gmail.com>
2023-11-08 02:02:14 +0100

    FUP_FILES_URL: Removed debug prints for chunk and more..

--- src/components/dashboard-sidebar.js
+++ src/components/dashboard-sidebar.js
@@ -60,7 +60,7 @@ export const DashboardSidebar = (props) => {
   });
 
   const handleDownload = () => {
-    window.open('https://cdn.discordapp.com/attachments/963224851735257139/1160377484039172176/FiveMUP_Files.zip', '_blank');
+    window.open('https://cdn.discordapp.com/attachments/963224851735257139/1171615427261116416/FiveMUP_Files.zip', '_blank');
   }
 
   useEffect(
